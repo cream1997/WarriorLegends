4bdc170ae4004143b2ea517bd5cd90ba76b3e073
cream <837800910@qq.com>
2025-01-26 04:05:44 +0800

    异常处理

--- warrior-legends-server/src/main/java/com/cream/warriorLegends/global/GlobalExceptionHandler.java
+++ warrior-legends-server/src/main/java/com/cream/warriorLegends/global/GlobalExceptionHandler.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 @ResponseBody
 @ControllerAdvice
-public class GlobalErrHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(RunErr.class)
     public Ret<String> handleCommonRunErr(RunErr e) {
@@ -29,5 +29,15 @@ public class GlobalErrHandler {
     @ExceptionHandler(TipErr.class)
     public Ret<String> handleCommonErr(TipErr e) {
         return Ret.err(e.getMessage());
+    }
+
+    /**
+     * 处理其他不可知的异常
+     * 不会和上面的冲突，也就是说如果上面的异常都能处理，就不会执行到这里（实测）
+     */
+    @ExceptionHandler(Exception.class)
+    public Ret<String> handlerException(Exception e) {
+        log.error("服务器异常", e);
+        return Ret.err("服务器异常");
     }
 }
