21afeb2f0a182863aa318d8e021680ef9d60d6eb
cream <837800910@qq.com>
2025-01-26 03:33:22 +0800

    跨域配置

---
+++ warrior-legends-server/src/main/java/com/cream/warriorLegends/config/mvc/MyWebMvcConfigurer.java
@@ -0,0 +1,43 @@
+package com.cream.warriorLegends.config.mvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+    /**
+     * SpringMVC整合FastFastJson
+     */
+    /*@Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+//        //自定义配置...
+//        FastJsonConfig config = new FastJsonConfig();
+//        config.setDateFormat("yyyy-MM-dd HH:mm:ss");
+//        config.setReaderFeatures(JSONReader.Feature.FieldBased, JSONReader.Feature.SupportArrayToBean);
+//        // ※最后一个是配置long类型转换为string，避免前端丢失精度
+//        config.setWriterFeatures(JSONWriter.Feature.WriteMapNullValue, JSONWriter.Feature.PrettyFormat, JSONWriter.Feature.WriteLongAsString);
+//
+//        converter.setFastJsonConfig(config);
+//        converter.setDefaultCharset(StandardCharsets.UTF_8);
+//        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+//        converters.add(0, converter);
+    }*/
+
+    /**
+     * 跨域配置
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 完全放开跨域
+        registry
+                .addMapping("/**") //添加映射路径
+                .allowCredentials(true) //是否发送Cookie
+                .allowedOriginPatterns("*") //允许跨域的域名，*代表允许任何域名
+                .allowedMethods("*") //允许任何方法（post、get等）
+                .exposedHeaders("*"); //允许任何头
+    }
+}
