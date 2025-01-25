ce63ac98d3e3d9d84b02ce41abd3353c581d78bb
cream <837800910@qq.com>
2025-01-25 21:16:04 +0800

    server init

---
+++ warrior-legends-server/src/main/java/com/cream/warriorLegends/WarriorLegendsServerApp.java
@@ -0,0 +1,13 @@
+package com.cream.warriorLegends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarriorLegendsServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarriorLegendsServerApp.class, args);
+	}
+
+}
---
+++ warrior-legends-server/src/test/java/com/cream/warriorLegends/WarriorLegendsServerAppTests.java
@@ -0,0 +1,13 @@
+package com.cream.warriorLegends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarriorLegendsServerAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
