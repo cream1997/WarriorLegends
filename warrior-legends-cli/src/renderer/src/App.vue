ea37b382ace53222dc1a1b21b8a55f667e346d91
cream <837800910@qq.com>
2025-01-25 23:43:13 +0800

    ..

--- warrior-legends-cli/src/renderer/src/App.vue
+++ warrior-legends-cli/src/renderer/src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
+import LoginPage from "@/pages/LoginPage.vue";
+
 const ipcHandle = () => window.electron.ipcRenderer.send("ping");
 </script>
 
 <template>
-  <div>
-    <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
-  </div>
+  <LoginPage></LoginPage>
 </template>
---
+++ warrior-legends-cli/src/renderer/src/pages/LoginPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <form>
+    <div>
+      <label for="username">用户名</label>
+      <input id="username" type="text" name="username" />
+    </div>
+    <div>
+      <label for="password">密码</label>
+      <input id="password" type="password" name="password" />
+    </div>
+
+    <button>登录</button>
+    <button>注册</button>
+  </form>
+</template>
+
+<style scoped></style>
