4787d7cf68f5c385d8cee691fcf4aa3ed4e989fe
cream <837800910@qq.com>
2025-01-26 06:51:19 +0800

    eslint..

--- warrior-legends-cli/.eslintrc.cjs
+++ warrior-legends-cli/.eslintrc.cjs
@@ -13,11 +13,12 @@ module.exports = {
   rules: {
     "vue/require-default-prop": "off",
     "vue/multi-word-component-names": "off",
-    //----自定义规则----
+    //----自定义规则, 为了前期开发方便，有些后期可以去掉----
     quotes: ["error", "double"], // 使用双引号
     semi: ["error", "always"], // 使用分号
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+    "vue/no-unused-vars": "off"
   }
 };
