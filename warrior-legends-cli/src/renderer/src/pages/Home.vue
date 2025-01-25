5b805b30b072bd4ac5bfbeef9fdd35cece7aab85
cream <837800910@qq.com>
2025-01-26 07:41:10 +0800

    地图绘制...

--- warrior-legends-cli/src/renderer/src/pages/Home.vue
+++ warrior-legends-cli/src/renderer/src/pages/Home.vue
@@ -1,7 +1,132 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, reactive, ref } from "vue";
+import { Player } from "@/obj/Player";
+
+const me: Player = {
+  id: "1",
+  name: "test",
+  x: 50,
+  y: 50
+};
+
+const objList = reactive<Player[]>([me]);
+
+const oneGridPx = 30;
+
+const mapMeta = reactive({
+  width: 25,
+  height: 25
+});
+
+const mapRef = ref<HTMLDivElement>();
+
+let self;
+
+onMounted(() => {
+  if (mapRef.value) {
+    mapRef.value.style.width = mapMeta.width * oneGridPx + "px";
+    mapRef.value.style.height = mapMeta.height * oneGridPx + "px";
+  }
+  fixSelf();
+  moveMap();
+  addMoveKeyListener();
+});
+
+function fixSelf() {
+  self = document.getElementById("1");
+  if (self) {
+    self.style.left = `calc(50vw - ${oneGridPx / 2}px)`;
+    self.style.top = `calc(50vh - ${oneGridPx / 2}px)`;
+  }
+}
+
+function moveMap() {
+  if (self) {
+    const rect = self.getBoundingClientRect();
+    const selfCenterLeft = rect.left + rect.width / 2;
+    const selfCenterTop = rect.top + rect.height / 2;
+    const mapLeft = selfCenterLeft - me.x;
+    const mapTop = selfCenterTop - me.y;
+    console.log(mapLeft);
+    if (mapRef.value) {
+      mapRef.value.style.left = mapLeft + "px";
+      mapRef.value.style.top = mapTop + "px";
+    }
+  }
+}
+
+let moving = false;
+
+function addMoveKeyListener() {
+  document.addEventListener("keydown", (e) => {
+    if (moving) {
+      return;
+    }
+    moving = true;
+    // 0.2s走一格300ms走30px
+    let moveLength = 0;
+    const task = setInterval(() => {
+      console.log("a");
+      moveLength += 1;
+      me.x += 1;
+      moveMap();
+      if (moveLength >= 30) {
+        clearInterval(task);
+        moving = false;
+      }
+    }, 10);
+  });
+}
+</script>
 
 <template>
-  <div>Home</div>
+  <div ref="mapRef" class="map">
+    <div
+      v-for="(_ignore1, rowIndex) in Array.from({ length: mapMeta.height })"
+      :key="rowIndex"
+      class="row"
+    >
+      <div
+        v-for="(_ignore2, columnIndex) in Array.from({
+          length: mapMeta.width
+        })"
+        :key="columnIndex"
+        class="grid"
+      ></div>
+    </div>
+  </div>
+  <div v-for="obj in objList" :id="obj.id" :key="obj.id" class="obj">
+    {{ obj.name }}
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.map {
+  position: fixed;
+  width: 80vw;
+  height: 80vh;
+  background: green;
+}
+
+.obj {
+  position: fixed;
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  background: red;
+  border: 1px solid black;
+  border-radius: 50%;
+}
+
+.row {
+  /*行高为0，消除行间距*/
+  line-height: 0;
+}
+
+.grid {
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  border: 1px solid black;
+}
+</style>
