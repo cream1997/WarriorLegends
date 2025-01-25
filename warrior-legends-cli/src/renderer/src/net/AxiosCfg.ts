41ccf6c415514bedbc88ba781d26fae7120b4d9b
cream <837800910@qq.com>
2025-01-26 03:57:19 +0800

    request的Content-Type设置

---
+++ warrior-legends-cli/src/renderer/src/net/AxiosCfg.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://127.0.0.1:8888/api/",
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+  }
+});
+
+function post(
+  url: string,
+  data?: any,
+  contentTypeIsJson: boolean = false,
+  config?: AxiosRequestConfig
+): Promise<any> {
+  if (contentTypeIsJson) {
+    /**
+     * 上面默认设置的是application/x-www-form-urlencoded;charset=UTF-8，
+     * 因为大部分请求都是这种格式，后端接口直接就接收了，不需要加@RequestBody
+     * 请求发送json格式，就在调用post方法时，将contentTypeIsJson设置为true
+     */
+    config = config ? config : {};
+    config.headers = config.headers ? config.headers : {};
+    config.headers["Content-Type"] = "application/json";
+  }
+  return axiosInstance.post(url, data, config);
+}
+
+export { axiosInstance as axios, post };
